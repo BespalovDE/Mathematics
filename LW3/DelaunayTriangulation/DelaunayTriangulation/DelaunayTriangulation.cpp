5e9f8e7342c679f5cea2acabc7515d1dca77097e
BespalovDE <dmitriy_bespalov12@mail.ru>
2022-05-02 21:01:55 +0300

    Добавлена программа для поиска количества остовных деревьев в графе.
    Добавлена часть программы Триангуляции Делоне(Не доделана).

---
+++ LW3/DelaunayTriangulation/DelaunayTriangulation/DelaunayTriangulation.cpp
@@ -0,0 +1,281 @@
+﻿// DelaunayTriangulation.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+
+struct Point
+{
+    double x;
+    double y;
+};
+
+struct Triangle
+{
+    Point a;
+    Point b;
+    Point c;
+};
+
+struct CircleAroundTriangle
+{
+    Triangle t;
+    Point center;
+    double radius;
+};
+
+using PointsVector = std::vector<Point>;
+using TriangleVector = std::vector<Triangle>;
+
+Triangle GetBigTriangle(PointsVector &pVector) // функция, находящая треугольник, содержащий все точки множества
+{
+    double minx = 1000000, maxx = -1000000, miny = 1000000, maxy = -1000000;
+    for (PointsVector::size_type i = 0; i != pVector.size(); i++)
+    {
+        if (minx > pVector[i].x)
+        {
+            minx = pVector[i].x;
+        }
+        if (miny > pVector[i].y)
+        {
+            miny = pVector[i].y;
+        }
+        if (maxx < pVector[i].x)
+        {
+            maxx = pVector[i].x;
+        }
+        if (maxy < pVector[i].y)
+        {
+            maxy = pVector[i].y;
+        }
+    }
+    double dx = maxx - minx;
+    double dy = maxy - miny;
+    double dxy = (dx > dy) ? dx : dy;
+    double midx = dx * 0.5 + minx;
+    double midy = dy * 0.5 + miny;
+    return
+        {
+            {midx - 10 * dxy, midy - 10 * dxy},
+            {midx, midy + 10 * dxy},
+            {midx + 10 * dxy, midy - 10 * dxy}
+        };
+}
+
+CircleAroundTriangle GetCircleAroundTriangle(Triangle triangle) // вычисление центра и радиуса описанной окружности вокруг треугольника
+{
+    double x1 = triangle.a.x, y1 = triangle.a.y;
+    double x2 = triangle.b.x, y2 = triangle.b.y;
+    double x3 = triangle.c.x, y3 = triangle.c.y;
+    double dy12 = abs(y1 - y2), dy23 = abs(y2 - y3);
+    double xc, yc;
+    if (dy12 < DBL_EPSILON) 
+    {
+        double m2 = -((x3 - x2) / (y3 - y2));
+        double mx2 = (x2 + x3) / 2, my2 = (y2 + y3) / 2;
+        xc = (x1 + x2) / 2, yc = m2 * (xc - mx2) + my2;
+    }
+    else if (dy23 < DBL_EPSILON) 
+    {
+        double m1 = -((x2 - x1) / (y2 - y1));
+        double mx1 = (x1 + x2) / 2, my1 = (y1 + y2) / 2;
+        xc = (x2 + x3) / 2, yc = m1 * (xc - mx1) + my1;
+    }
+    else 
+    {
+        double m1 = -((x2 - x1) / (y2 - y1)), m2 = -((x3 - x2) / (y3 - y2));
+        double mx1 = (x1 + x2) / 2, my1 = (y1 + y2) / 2;
+        double mx2 = (x2 + x3) / 2, my2 = (y2 + y3) / 2;
+        xc = (m1 * mx1 - m2 * mx2 + my2 - my1) / (m1 - m2);
+        if (dy12 > dy23)
+        {
+            yc = m1 * (xc - mx1) + my1;
+        }
+        else
+        {
+            yc = m2 * (xc - mx2) + my2;
+        }
+    }
+    double dx = x2 - xc, dy = y2 - yc;
+    return { triangle,  { xc, yc }, dx * dx + dy * dy };
+}
+
+void DelecteMultiplesEdgec(/*std::vector<double>& edges*/PointsVector &edges ) // удаление кратных ребер
+{
+    for (int j = edges.size() - 1; j >= 0;)
+    {
+        double b = edges[j].x;
+        double a = edges[j].y;
+        j--;
+        double n, m;
+        for (int i = j; i >= 0;)
+        {
+            n = edges[i].x;
+            m = edges[i].y;
+            i--;
+            auto vBegin = edges.cbegin();
+            if (a == m && b == n)
+            {
+                edges.erase(vBegin + j + 1);
+                break;
+            }
+            vBegin = edges.cbegin();
+            if (a == n && b == m)
+            {
+                edges.erase(vBegin + i + 1);
+                break;
+            }
+        }
+    }
+    
+    /*for (int j = edges.size() - 1; j >= 0;)
+    {
+        double b = edges[j]; 
+        j--;
+        double a = edges[j]; 
+        j--;
+        double n, m;
+        for (double i = j; i >= 0;) 
+        {
+            n = edges[i]; 
+            i--;
+            m = edges[i]; 
+            i--;
+            auto vBegin = edges.cbegin();
+            if (a == m && b == n) 
+            {
+                auto vEnd = vBegin + j + 3;
+                edges.erase(vBegin + j + 1, vEnd);
+                edges.erase(vBegin + i + 1, vEnd);
+                break;
+            }
+            vBegin = edges.cbegin();
+            if (a == n && b == m) 
+            {
+                auto vEnd = vBegin + j + 3;
+                edges.erase(vBegin + j + 1, vEnd);
+                edges.erase(vBegin + i + 1, vEnd);
+                break;
+            }
+        }
+    }*/
+}
+
+TriangleVector Triangulate(PointsVector &pVector) {
+    int n = pVector.size();
+    TriangleVector triangleVector;
+    if (n < 3)
+    {
+        return triangleVector; // треугольников нет
+    }
+    PointsVector points = pVector; // копия массива
+
+    std::vector<int> indexes;
+    for (int i = 0; i < n; i++) 
+        indexes.emplace_back(i);
+    //sort(indexes.begin(), indexes.end(), [](Point lhs, Point rhs) {return lhs.x < rhs.x; });
+
+    // находим треугольник, содержащий все точки, и вставлем его в конец массива с вершинами
+    Triangle bigTriangle = GetBigTriangle(pVector);
+    points.emplace_back(bigTriangle.a);
+    points.emplace_back(bigTriangle.b);
+    points.emplace_back(bigTriangle.c);
+
+    std::vector<CircleAroundTriangle> triangleStructs = { GetCircleAroundTriangle({ points[n], points[n + 1], points[n + 2] }) };
+    std::vector<CircleAroundTriangle> ans;
+    PointsVector edges;
+    
+    // перебираем все точки
+    for (int i = indexes.size() - 1; i >= 0; i--) 
+    {
+        
+        // перебираем все треугольники
+        // если точка находится внутри треугольника, то нужно его удалить
+        for (int j = triangleStructs.size() - 1; j >= 0; j--)
+        {
+            // если точка справа от описанной окружности, то треугольник проверять больше не нужно
+            // точки отсортированы и поэтому тоже будут справа
+            double dx = points[indexes[i]].x - triangleStructs[j].center.x;
+            if (dx > 0 && dx * dx > triangleStructs[j].radius) 
+            {
+                ans.emplace_back(triangleStructs[j]);
+                //cur_points.splice(j, 1);
+                auto vBegin = triangleStructs.cbegin();
+                triangleStructs.erase(vBegin + j);
+                continue;
+            }
+            
+            // если точка вне окружности, то треугольник изменять не нужно
+            double dy = points[indexes[i]].y - triangleStructs[j].center.y;
+            if (dx * dx + dy * dy - triangleStructs[j].radius > DBL_EPSILON) 
+            {
+                continue;
+            }
+            // удаляем треугольник и добавляем его стороны в список ребер
+
+            /*edges.emplace_back(
+                 triangleStructs[j].t.a, triangleStructs[j].t.b,
+                 triangleStructs[j].t.b, triangleStructs[j].t.c,
+                 triangleStructs[j].t.c, triangleStructs[j].t.a
+            );*/
+            edges.emplace_back(triangleStructs[j].t.a);
+            edges.emplace_back(triangleStructs[j].t.b);
+            edges.emplace_back(triangleStructs[j].t.c);
+
+            auto vBegin = triangleStructs.cbegin();
+            triangleStructs.erase(vBegin + j);
+            //cur_points.splice(j, 1);
+        }
+        // удаляем кратные ребра
+        DelecteMultiplesEdgec(edges);
+        // создаем новые треугольники последовательно по списку ребер
+        for (int j = edges.size() - 1; j >= 0;)
+        {
+            if (j >= 2)
+            {
+                Point b = edges[j - 1]; //{ edges[j], edges[j - 1] };
+                Point a = edges[j];//{ edges[j] - 2, edges[j - 3] };
+                triangleStructs.emplace_back(GetCircleAroundTriangle({ a, b,  pVector[indexes[i]] }));
+            }
+            j = j - 2;
+        }
+        edges.clear();
+        //edges = [];
+    }
+    
+
+    TriangleVector tr;
+    // формируем массив с триангуляцией
+    for (int i = triangleStructs.size() - 1; i >= 0; i--) {
+        ans.emplace_back(triangleStructs[i]);
+    }
+    for (int i = 0; i < ans.size(); i++)
+    {
+        //if (ans[i].a < n && ans[i].b < n && ans[i].c < n) {
+            //tr.emplace_back(ans[i].t.a, ans[i].t.b, ans[i].t.c);
+        tr.emplace_back(ans[i].t);
+        //}
+    }
+    return tr;
+}
+
+
+int main()
+{
+    PointsVector pVector = { {0.0,0.0}, {1.0,1.0}, {0.0,1.0}, {1.0,0.0} };
+    TriangleVector triangleVector = Triangulate(pVector);
+    std::cout << triangleVector[0].a.x << " " << triangleVector[0].a.y << " " << triangleVector[0].b.x << " " << triangleVector[0].b.y << " " << triangleVector[0].c.x << " " << triangleVector[0].c.y;
+}
+
+// формируем массив с триангуляцией
+    /*for (int i = triangleStructs.size() - 1; i >= 0; i--) {
+        ans.emplace_back(triangleStructs[i]);
+    }
+    for (int i = 0; i < ans.size(); i++)
+    {
+        //if (ans[i].a < n && ans[i].b < n && ans[i].c < n) {
+            tr.emplace_back(ans[i].t.a, ans[i].t.b, ans[i].t.c);
+        //}
+    }*/
---
+++ LW3/SpanningTrees/SpanningTrees/SpanningTrees/SpanningTrees.cpp
@@ -0,0 +1,196 @@
+﻿// SpanningTrees.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <optional>
+#include <boost/numeric/ublas/matrix.hpp>
+#include <boost/numeric/ublas/io.hpp>
+
+using Matrix = boost::numeric::ublas::matrix<double>;
+
+struct Args
+{
+	std::string fileName;
+};
+
+std::optional<Args> ParseArgs(int argc, char* argv[], std::ostream& outStream)
+{
+	if (argc != 2)
+	{
+		outStream << "Invalid arguments count.\n";
+		outStream << "Usage: SpanningTrees.exe <input file name>\n";
+		return std::nullopt;
+	}
+	Args args;
+	args.fileName = argv[1];
+	return args;
+}
+
+bool FillMatrix(Matrix &m, int matrixLen, std::istream &inputFile)
+{
+	std::string line;
+
+	for (int i = 0; i < matrixLen; i++)
+	{
+		for (int j = 0; j < matrixLen; j++)
+		{
+			inputFile >> line;
+			try
+			{
+				m(i, j) = std::atof(line.c_str());
+			}
+			catch (const std::exception& /* error*/)
+			{
+				return false;
+			}
+		}
+	}
+	return true;
+}
+
+bool FillKirchhoffMatrices(Matrix &inputMatrix, Matrix &kirchhofftMatrix)
+{
+	for (unsigned  i = 0; i < inputMatrix.size1(); i++)
+	{
+		double relations = 0.0;
+		for (unsigned j = 0; j < inputMatrix.size1(); j++)
+		{
+			if (inputMatrix(i, j) > DBL_EPSILON)
+			{
+				double d1 = inputMatrix(i, j);
+				relations++;
+				kirchhofftMatrix(i, j) = -1.0;
+			}
+			else
+			{
+				kirchhofftMatrix(i, j) = 0.0;
+			}
+		}
+		if (relations > DBL_EPSILON)
+		{
+			kirchhofftMatrix(i, i) = relations;
+		}
+		else
+		{
+			return false;
+		}
+	}
+	return true;
+}
+
+void PrintMatrix(Matrix &x)
+{
+	for (int i = 0; i < x.size1(); i++)
+	{
+		for (int j = 0; j < x.size1(); j++)
+			std::cout << x(i,j) << "\t";
+		std::cout << "\n";
+	}
+}
+
+void ConvertRow(Matrix &kirchhofftMatrix, int i, int j)
+{
+	double value = kirchhofftMatrix(i, j);
+	double valueAbove = kirchhofftMatrix(j, j);
+	double divider = -kirchhofftMatrix(i, j) / kirchhofftMatrix(j, j);
+	for (int x = j; x < kirchhofftMatrix.size1(); x++)
+	{
+		kirchhofftMatrix(i, x) = kirchhofftMatrix(i, x) + kirchhofftMatrix(j, x) * divider;
+	}
+}
+
+int GetSpanningTreesNumber(Matrix &kirchhofftMatrix)
+{
+	Matrix oprMatrix(kirchhofftMatrix.size1() - 1, kirchhofftMatrix.size1() - 1);
+	for (unsigned i = 0; i < kirchhofftMatrix.size1() - 1; i++)
+	{
+		for (unsigned j = 0; j < kirchhofftMatrix.size1() - 1; j++)
+		{
+			oprMatrix(i, j) = kirchhofftMatrix(i, j);
+		}
+	}
+	for (unsigned j = 0; j < oprMatrix.size1(); j++)
+	{
+		if (j + 1 < oprMatrix.size1())
+		{
+			for (unsigned i = j + 1; i < oprMatrix.size1(); i++)
+			{
+				if (abs(oprMatrix(i, j)) > DBL_EPSILON)
+				{
+					ConvertRow(oprMatrix, i, j);
+					/*PrintMatrix(oprMatrix); // отладка
+					std::cout << std::endl;*/
+				}
+			}
+		}
+	}
+	double result = 0;
+	for (unsigned i = 0; i < oprMatrix.size1(); i++)
+	{
+		if (i == 0)
+		{
+			result = oprMatrix(i, i);
+		}
+		else
+		{
+			result *= oprMatrix(i, i);
+		}
+	}
+	//std::cout << result << std::endl;
+	return result;
+}
+
+
+int main(int argc, char* argv[])
+{
+	auto args = ParseArgs(argc, argv, std::cout);
+	std::ifstream inputFile(args->fileName);
+	int matrixLen = 0;
+	if (inputFile.is_open() && inputFile.peek() != EOF)
+	{
+		std::string line;
+		inputFile >> line;
+		try
+		{
+			matrixLen = std::stoi(line);
+		}
+		catch (const std::exception& /* error*/)
+		{
+			std::cout << "File has is not numeric values!" << std::endl;
+			return 1;
+		}
+		if (matrixLen > 1)
+		{
+			Matrix inputMatrix(matrixLen, matrixLen);
+			if (!FillMatrix(inputMatrix, matrixLen, inputFile))
+			{
+				std::cout << "File has is not numeric values!" << std::endl;
+				return 1;
+			}
+			Matrix kirchhofftMatrix(matrixLen, matrixLen);
+			if (FillKirchhoffMatrices(inputMatrix, kirchhofftMatrix))
+			{
+				std::cout << "There are " << GetSpanningTreesNumber(kirchhofftMatrix) << " spanning trees.";
+				//std::cout << inputMatrix << std::endl;
+				//std::cout << std::endl;
+				//std::cout << kirchhofftMatrix << std::endl;
+			}
+			else
+			{
+				std::cout << "There are more than one graph!" << std::endl;
+				return 1;
+			}
+		}
+		else
+		{
+			std::cout << "Matrix length < 2!" << std::endl;
+			return 1;
+		}
+	}
+	else
+	{
+		std::cout << "Empty file!" << std::endl;
+		return 1;
+	}
+}
